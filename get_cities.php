ba88af984b80f46ea024e1af152881e9da1d06be
Dabhi Nayan <dabhinayan41@gmail.com>
2024-10-14 21:56:20 +0530

    added country, state, city dropdown

---
+++ get_cities.php
@@ -0,0 +1,21 @@
+<?php
+include('connection.php');
+
+if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true){
+    header('location: login-form.php');
+    exit;
+}
+
+if (isset($_POST['state_id']) && isset($_POST['country_id'])) {
+    $stateId = $_POST['state_id'];
+    
+    $query = "SELECT * FROM cities WHERE state_id = $stateId";
+    $result = mysqli_query($conn, $query);
+    
+    echo '<option value="">Select City</option>';
+    
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<option value='{$row['id']}'>{$row['name']}</option>";
+    }
+}
+?>
